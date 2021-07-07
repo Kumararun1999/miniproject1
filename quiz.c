edaa915e1b0f308be4a9e04b093b9a714ed55a25
kumararun <arunkumarhr.smg@gmail.com>
2021-07-07 13:16:05 +0530

    second commit

---
+++ quiz.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+int main()
+     {
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
+     mainhome:
+     system("cls");
+     printf("\t\t\tC PROGRAM QUIZ GAME\n");
+     printf("\n\t\t________________________________________");
+
+     printf("\n\t\t\t   WELCOME ");
+     printf("\n\t\t\t      to ");
+     printf("\n\t\t\t   THE GAME ");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t   BECOME A MILLIONAIRE!!!!!!!!!!!    ") ;
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t > Press S to start the game");
+     printf("\n\t\t > Press V to view the highest score  ");
+     printf("\n\t\t > Press R to reset score");
+     printf("\n\t\t > press H for help            ");
+     printf("\n\t\t > press Q to quit             ");
+     printf("\n\t\t________________________________________\n\n");
+     choice=toupper(getch());
+     if (choice=='V')
+	{
+	show_record();
+	goto mainhome;
+	}
+     else if (choice=='H')
+	{
+	help();getch();
+	goto mainhome;
+	}
+	else if (choice=='R')
+	{reset_score();
+	getch();
+	goto mainhome;}
+	else if (choice=='Q')
+	exit(1);
+    else if(choice=='S')
+    {
+     system("cls");
+
+    printf("\n\n\n\n\n\n\n\n\n\n\t\t\tResister your name:");
+     gets(playername);
